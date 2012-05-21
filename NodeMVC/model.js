616d2e128548bde2e3569c22a14f6f514656352f
kevin cain <kevinmcain@hotmail.com>
2012-05-20 18:03:37 -0700

    removed code that was no longer needed

--- NodeMVC/model.js
+++ NodeMVC/model.js
@@ -1,34 +1,13 @@
-// NodeMVC model extension
+// NodeMVC model base class
 
-var childProcess = require('child_process');
+var model = function () {
 
-// for running model functions (business logic) asynchronously
-// should send in the file that needs to be required, and function name that it needs
-var runWorkerAsync = function(response) {
+	var that = {};
 	
-	var childCallback = function (m) {
-		// its probably more appropriate to return the data to the controller
-		// and the controller assigns it to the view. response not used here
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write(m.returnValue);
-		response.end();
-    }
-	// create a child worker process 
-	var worker = childProcess.fork(process.cwd() + '/worker.js');
-	worker.on('message', childCallback);
-	// send should send in the file that needs to be 
-	// required and the function that needs to be executed
-	worker.send({ action: 'start' });
+	// TODO: still thinking about what might be needed here. 
+
+	return that;
+
 };
 
-// this would be as if the user defined it 
-var runWorker = function(response) {
-	// its probably more appropriate to return the data to the controller
-	// and the controller assigns it to the view. response not used here
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello Worker");
-	response.end();
-};
-
-exports.runWorker = runWorker;
-exports.runWorkerAsync = runWorkerAsync;
+module.exports = model;
