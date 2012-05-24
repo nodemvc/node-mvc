32639e54ca4ae49acf4f2a4fd09dd8053e3ef289
kevin cain <kevinmcain@hotmail.com>
2012-05-23 18:57:34 -0700

    added controller with use of model example

--- NodeMVC/NodeMVC.js
+++ NodeMVC/NodeMVC.js
@@ -1,7 +1,38 @@
 // This starts the server and allows us to wire the other modules together
 
-var controller = require("./controller")();
+var controller = require("./controller");
 var server = require("./server");
 var model = require("./model");
 
-server.run(controller, 8888);
+// here is an example of a developer defined controller with the use of a model.
+var accounts = function() {
+		
+	var that = controller(); // <-- inherit the base controller class
+		
+	// developer defines an account model object 
+	var accountModel = function() {
+		var that = model(); // <-- inherit the base model class
+		that.fname = "";
+		that.lname = "";
+		that.password = "";
+		return that;
+	};
+		
+	// developer defines a logon function for this account controller
+	that.logon = function() {
+		return that.view();
+	};
+		
+	// developer defines a logon attempt function for this account controller
+	that.logonAttempt = function(accountModel) {
+		// logic to validate the user would go here  
+		console.log("validating " + accountModel.firstName + ', ' + accountModel.lastName)
+		return that.view();
+	};
+		
+	// developer assigns the accountModel function object to the logonAttempt function
+	that.logonAttempt.model = accountModel;
+	return that;
+};
+
+server.run(accounts, 8888);
