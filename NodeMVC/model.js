c8a39c90a3f780eca0c41d3da4716381b9936a21
kevin cain <kevinmcain@hotmail.com>
2012-05-26 22:02:18 -0700

    added model property func and changed setParameters to bindModel

--- NodeMVC/model.js
+++ NodeMVC/model.js
@@ -1,26 +1,43 @@
 // NodeMVC model base class
 
-var model = function () {
-
+var model = function (spec) {
+	
 	var that = {};
 	
-	// still need state management, which might change this from a function object to something else
-	
-	// attempts to map the query parameters from the request to the members of the model
-	var setParams = function (query) {
-		
-		// for all query params, see if there is a coresponding member, otherwise throw
-		// an error back to the controller, so that the controller can notify the server
+	// expecting 'url.parse(request.url, true).query' url
+	var bindModel = function (query) {
 		for(indx in query) {
-			if (that.hasOwnProperty(indx)) { 
-				that[indx] = query[indx];
+			if (that.hasOwnProperty(indx) && typeof that[indx].setValue === "function") { 
+				that[indx].setValue(query[indx]); 
 			} else {
 				throw new Error(indx + " could not be mapped to a member of the model");
 			}
 		}
 	};
 	
-	that.setParameters = setParams;
+	// model property is used to represent data in the model
+	var mProperty = function(displayName, required, dataType) {
+		var that = {};
+		that.prop = "";
+		// specifies the display name for the property
+		that.displayName = displayName;
+		// specifies that a data field value is required
+		that.required = required;
+		// specifies the name of an additional type to associate with a data field
+		that.dataType = dataType;
+	
+		that.getValue = function() {
+			return that.value;
+		};
+		
+		that.setValue = function(val) {
+			that.value = val;
+		};
+		
+		return that;
+	};
+	that.mProperty = mProperty;
+	that.bindModel = bindModel;
 	return that;
 };
 
