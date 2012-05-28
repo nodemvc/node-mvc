60c12722b4e794f02ca559adfc173e8a68818678
kevin cain <kevinmcain@hotmail.com>
2012-05-28 16:47:57 -0700

    updated developer objects

--- NodeMVC/NodeMVC.js
+++ NodeMVC/NodeMVC.js
@@ -10,30 +10,57 @@ var accounts = function() {
 	var that = controller(); // <-- inherit the base controller class
 		
 	// developer defines an account model object 
-	var accountModel = function() {
+	var logonModel = function() {
 		var that = model(); // <-- inherit the base model class
-		that.addProperty("fname", {displayName: "First Name", required: true});
-		that.addProperty("lname", {displayName: "Last Name", required: true});
+		that.addProperty("username", {displayName: "User name", required: true});
 		that.addProperty("password", {displayName: "Password", required: true, dataType: "password"});
+		that.addProperty("rememberme", {displayName: "Remember Me"});
 		return that;
 	};
 		
 	// developer defines a logon function for this account controller
-	that.logon = function() {
-		return that.view();
+	that.logon = function(logonModel) {
+		if (logonModel.clientBound === false) {
+			return that.view();
+		}
+		return that.redirectToAction("index" , "home");
 	};
+	
+	// developer assigns the logonModel function object to the logon function
+	that.logon.model = logonModel;
 		
-	// developer defines a logon attempt function for this account controller
-	that.logonAttempt = function(accountModel) {
-		// logic to validate the user would go here  
-		console.log("validating " + accountModel.fname.getValue() + ', ' + accountModel.lname.getValue());
-		return that.view();
-	};
-		
-	// developer assigns the accountModel function object to the logonAttempt function
-	that.logonAttempt.model = accountModel;
 	return that;
 };
+
+// here is an example of a developer defined controller with the use of a model.
+var userInfo = function() {
+		
+	var that = controller(); // <-- inherit the base controller class
+		
+	// developer defines an info model object 
+	var infoModel = function() {
+		var that = model(); // <-- inherit the base model class
+		that.addProperty("username", {displayName: "User name", required: true});
+		that.addProperty("password", {displayName: "Password", required: true, dataType: "password"});
+		that.addProperty("rememberme", {displayName: "Remember Me"});
+		return that;
+	};
+		
+	// developer defines an info function for this userInfo controller
+	that.info = function(infoModel) {
+		if (infoModel.clientBound === false) {
+			return that.view();
+		};
+		// retrieve model info for the user
+		return that.view();
+	};
+	
+	// developer assigns the infoModel function object to the infoUpdate function
+	that.info.model = infoModel;
+		
+	return that;
+};
+
 
 server.addController(accounts(), "accounts");
 server.run(8888);
